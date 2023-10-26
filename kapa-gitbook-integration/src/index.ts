8c2b98f7783357bef74578fbad7c24048614a0cc
Paul Kuhle <paul.kuhle@hotmail.de>
2023-10-26 13:10:10 +0200

    add remaining variables, description, etc.

--- kapa-gitbook-integration/src/index.ts
+++ kapa-gitbook-integration/src/index.ts
@@ -3,19 +3,31 @@ import {
   FetchPublishScriptEventCallback,
   RuntimeContext,
   RuntimeEnvironment,
-} from '@gitbook/runtime';
+} from "@gitbook/runtime";
 
-import script from './script.raw.js';
+import script from "./script.raw.js";
 
 type KapaRuntimeContext = RuntimeContext<
   RuntimeEnvironment<
-      {},
-      {
-        website_id?: string;
-        project_name?: string;
-        project_color?: string;
-        project_logo?: string;
-      }
+    {},
+    {
+      website_id?: string;
+      project_name?: string;
+      project_color?: string;
+      project_logo?: string;
+      button_position_top?: string;
+      button_position_bottom?: string;
+      button_position_left?: string;
+      button_position_right?: string;
+      button_hide?: string;
+      modal_override_open_class?: string;
+      modal_open_by_default?: string;
+      modal_search_placeholder?: string;
+      modal_title?: string;
+      modal_disclaimer?: string;
+      modal_hide_image?: string;
+      button_text?: string;
+    }
   >
 >;
 
@@ -25,30 +37,58 @@ export const handleFetchEvent: FetchPublishScriptEventCallback = async (
 ) => {
   const website_id = environment.spaceInstallation.configuration.website_id;
   const project_name = environment.spaceInstallation.configuration.project_name;
-  const project_color = environment.spaceInstallation.configuration.project_color;
+  const project_color =
+    environment.spaceInstallation.configuration.project_color;
   const project_logo = environment.spaceInstallation.configuration.project_logo;
 
+  const config = {
+    button_position_top:
+      environment.spaceInstallation.configuration.button_position_top,
+    button_position_bottom:
+      environment.spaceInstallation.configuration.button_position_bottom,
+    button_position_left:
+      environment.spaceInstallation.configuration.button_position_left,
+    button_position_right:
+      environment.spaceInstallation.configuration.button_position_right,
+    button_hide: environment.spaceInstallation.configuration.button_hide,
+    modal_override_open_class:
+      environment.spaceInstallation.configuration.modal_override_open_class,
+    modal_open_by_default:
+      environment.spaceInstallation.configuration.modal_open_by_default,
+    modal_search_placeholder:
+      environment.spaceInstallation.configuration.modal_search_placeholder,
+    modal_title: environment.spaceInstallation.configuration.modal_title,
+    modal_disclaimer:
+      environment.spaceInstallation.configuration.modal_disclaimer,
+    modal_hide_image:
+      environment.spaceInstallation.configuration.modal_hide_image,
+    button_text: environment.spaceInstallation.configuration.button_text,
+  };
+
   if (!website_id || !project_name || !project_color || !project_logo) {
-      return;
+    return;
   }
 
-  const parsedScript = script.replace(
-    '<WEBSITE_ID>', website_id
-  ).replace(
-    '<PROJECT_NAME>', project_name
-  ).replace(
-    '<PROJECT_COLOR>', project_color
-  ).replace(
-    '<PROJECT_LOGO>', project_logo
-  )
+  let parsedScript = script
+    .replace("<WEBSITE_ID>", website_id)
+    .replace("<PROJECT_NAME>", project_name)
+    .replace("<PROJECT_COLOR>", project_color)
+    .replace("<PROJECT_LOGO>", project_logo);
+
+  for (const [key, value] of Object.entries(config)) {
+    if (value) {
+      parsedScript = parsedScript.replace(`<${key.toUpperCase()}>`, value);
+    } else {
+      parsedScript = parsedScript.replace(`<${key.toUpperCase()}>`, "undefined");
+    }
+  }
 
   return new Response(parsedScript, {
-      headers: {
-          'Content-Type': 'application/javascript',
-          'Cache-Control': 'max-age=604800',
-      },
+    headers: {
+      "Content-Type": "application/javascript",
+      "Cache-Control": "max-age=604800",
+    },
   });
-
 };
 
 export default createIntegration<KapaRuntimeContext>({
--- kapa-gitbook-integration/src/script.raw.js
+++ kapa-gitbook-integration/src/script.raw.js
@@ -8,6 +8,21 @@
     const PROJECT_COLOR = '<PROJECT_COLOR>';
     const PROJECT_LOGO = '<PROJECT_LOGO>';
 
+    const config = {
+        BUTTON_POSITION_TOP: '<BUTTON_POSITION_TOP>',
+        BUTTON_POSITION_BOTTOM: '<BUTTON_POSITION_BOTTOM>',
+        BUTTON_POSITION_LEFT: '<BUTTON_POSITION_LEFT>',
+        BUTTON_POSITION_RIGHT: '<BUTTON_POSITION_RIGHT>',
+        BUTTON_HIDE: '<BUTTON_HIDE>',
+        MODAL_OVERRIDE_OPEN_CLASS: '<MODAL_OVERRIDE_OPEN_CLASS>',
+        MODAL_OPEN_BY_DEFAULT: '<MODAL_OPEN_BY_DEFAULT>',
+        MODAL_SEARCH_PLACEHOLDER: '<MODAL_SEARCH_PLACEHOLDER>',
+        MODAL_TITLE: '<MODAL_TITLE>',
+        MODAL_DISCLAIMER: '<MODAL_DISCLAIMER>',
+        MODAL_HIDE_IMAGE: '<MODAL_HIDE_IMAGE>',
+        BUTTON_TEXT: '<BUTTON_TEXT>',
+    };
+
     var l = function () {
         var s = d.createElement('script');
         s.type = 'text/javascript';
@@ -17,6 +32,13 @@
         s.setAttribute("data-project-name", PROJECT_NAME);
         s.setAttribute("data-project-color", PROJECT_COLOR);
         s.setAttribute("data-project-logo", PROJECT_LOGO);
+
+        for (const [key, value] of Object.entries(config)) {
+            const kebabCaseKey = key.replace("_", "-").toLowerCase();
+            if (value !== 'undefined') {
+                s.setAttribute(`data-${kebabCaseKey}`, value);
+            }
+        }
 
         var x = d.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
